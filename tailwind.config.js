ebeea669263e919e1adfde0de78cc86b874bb4e9
Sparsh Agarwal <sparshaga25@gmail.com>
2021-09-05 07:54:17 +0530

    Added Color Palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                brand: '#8a00d4',
+                primary: '#d527b7',
+                primaryLight: '#f782c2',
+                secondary: '#f9c46b',
+                accent: '#e3e3e3',
+            },
+        },
     },
     variants: {
         extend: {},
